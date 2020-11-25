7bbf22539c7c85f6da12c9763568984ad2512c79
ZzDamon <damonlepirda@gmail.com>
2020-11-25 14:36:22 +0200

    added an array of restaurant objects and made functions to add and get them from local storage

---
+++ index.js
@@ -0,0 +1,160 @@
+// cream un obiect global, numit APP
+window.APP = {};
+// index.js fiind primul fisier care se incarca, mainPage va fi available in toate celelalte fisiere
+window.mainPage = document.querySelector('#root');
+window.mainPage.innerHTML+=`<div class="header"></div>
+<div class="container"></div>`;
+
+APP.restaurants=[
+    {
+        image:'https://i.pinimg.com/originals/d7/17/23/d71723202ccde3f10e638a5da6dba237.jpg',
+        name:"Domino's Pizza",
+        minPrice:40,
+        deliveryPrice:5,
+        description:"Italian style pizza",
+        priceRange:'$$',
+        deliveryTime:'40min',
+        menu:{
+            popular: [
+                {
+                    name:"Pizza Extravaganzza",
+                    price: 25
+            },
+            {
+                name:"Pizza Domino's Special",
+                price: 32
+            }
+        ],
+            pizza:[
+            {
+                name:'Pizza Margherita',
+                price:25
+            },
+            {
+                name:'Pizza Cuattro Stagioni',
+                price:28
+            }
+        ],
+            pasta:[
+            {
+                name:'Carbonara Pasta',
+                price:24.5
+            },
+            {
+                name:'Pollo Pasta',
+                price: 24.5
+            }
+        ]
+        }
+    },
+
+    {
+        image:'https://upload.wikimedia.org/wikipedia/commons/f/ff/Kentucky_Fried_Chicken_201x_logo.svg',
+        name:"KFC",
+        minPrice:30,
+        deliveryPrice:9,
+        description:"American burgers, Fried Chicken",
+        priceRange:'$',
+        deliveryTime:'25min',
+        menu:{
+            popular: [
+                {
+                    name:"Christmas Box",
+                    price: 30
+            },
+            {
+                name:"Christmas Bucket",
+                price: 50
+            }
+        ],
+            buckets:[
+            {
+                name:'Giant Bucket',
+                price:80
+            },
+            {
+                name:'Smart Bucket Plus',
+                price:70
+            }
+        ],
+            burgers:[
+            {
+                name:'Booster Menu',
+                price:20.5
+            },
+            {
+                name:'Double Crispy Burger Menu',
+                price: 20.5
+            }
+        ]
+        }
+    },
+
+    {
+        image:'https://images.safe.com/logos/customers/mcdonalds.png',
+        name:"McDonald's",
+        minPrice:25,
+        deliveryPrice:0,
+        description:"American Burgers",
+        priceRange:'$',
+        deliveryTime:'30min',
+        menu:{
+            popular: [
+                {
+                    name:"Big Mac Menu",
+                    price: 17.9
+            },
+            {
+                name:"Double Cheesburger Menu",
+                price: 17.9
+            }
+        ],
+            premium:[
+            {
+                name:'Crispy Chicked McWrap Menu',
+                price:21
+            },
+            {
+                name:'Royal Deluxe Menu',
+                price:21
+            }
+        ],
+            clasic:[
+            {
+                name:'McChicken Menu',
+                price:17.9
+            },
+            {
+                name:'McNuggets Meni',
+                price: 27.9
+            }
+        ]
+        }
+    }
+]
+
+//add to local storage
+APP.addRestaurants = (restaurants) => {
+	localStorage.setItem("restaurants", JSON.stringify(restaurants));
+}
+// pentru a extrage vectorul de produse, luam valoarea corespunzatoare cheii "products"
+// apoi o parsam, pentru a deveni iar vector de obiecte, si o returnam 
+APP.getRestaurants = () => {
+	const restaurants = localStorage.getItem("restaurants");
+	return JSON.parse(restaurants);
+};
+// daca nu avem nimic in baza de date
+// introducem vectorul de produse in localStorage
+if (APP.getRestaurants() === null){
+	APP.addRestaurants(APP.restaurants);
+}
+
+
+function startRendering() {
+	// "randam" lista de produse, trimitand ca parametru APP.products (incarcat mai sus)
+	APP.renderRestaurantList(APP.getRestaurants());
+}
+
+// DUPA ce s-au incarcat TOATE scripturile, apelam functia startRendering
+// facem asta pentru ca la momentul incarcarii lui index.js, nu stim cine renderProductList()
+window.addEventListener('load', startRendering);
