c6512aec0757fb236cb960adc891f45e1f7574ea
ZzDamon <damonlepirda@gmail.com>
2020-11-29 20:48:51 +0200

    made cuter icons for map

--- geolocation.js
+++ geolocation.js
@@ -2,9 +2,18 @@ function getLocation(map) {
     if (navigator.geolocation) {
       return navigator.geolocation.getCurrentPosition(function(pos){
         //   console.log(pos.coords)
+        let icon={
+           url: "https://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/house-icon.png",
+            // url:"https://cdn.iconscout.com/icon/free/png-256/motorcycle-delivery-2127501-1792684.png",
+            scaledSize: new google.maps.Size(40,40),
+            origin: new google.maps.Point(0,0), // origin
+            anchor: new google.maps.Point(0, 0) // anchor
+  
+        }
         const markHome =new google.maps.Marker({
             position:{lat:pos.coords.latitude, lng:pos.coords.longitude},
-            map:map
+            map:map,
+            icon:icon
         });
 
         //   return pos.coords;
@@ -19,14 +28,23 @@ function getLocation(map) {
       
       page.innerHTML+=`<div id="map"> </div>`;
       const options ={
-          zoom:15,
+          zoom:13,
           center: {lat:APP.cart[0].restaurant.address[0], lng:APP.cart[0].restaurant.address[1]
           }
       }
       let map= new google.maps.Map(document.querySelector("#map"),options);
+      let icon={
+          //https://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/house-icon.png
+          url:"https://cdn.iconscout.com/icon/free/png-256/motorcycle-delivery-2127501-1792684.png",
+          scaledSize: new google.maps.Size(50,50),
+          origin: new google.maps.Point(0,0), // origin
+          anchor: new google.maps.Point(0, 0) // anchor
+
+      }
       let markRest= new google.maps.Marker({
           position:{lat:APP.cart[0].restaurant.address[0], lng:APP.cart[0].restaurant.address[1]},
-          map:map
+          map:map,
+          icon:icon
       });
       let homeCoordinates= getLocation(map);
       console.log(homeCoordinates);
